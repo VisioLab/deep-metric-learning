fixed some typos and bugs in kNN and added model class weighting

data/hdf5_loader.py
@@ -40,16 +40,18 @@ class FoodData(torch.utils.data.Dataset):
 
 class CustomSampler(torch.utils.data.Sampler):
 
-    def __init__(self, M, indices, labels, batch_size):
+    def __init__(self, M, indices, labels, batch_size, class_weights=None):
         """
         this is a custom sampler to try to extend Kevins sampler.
         Change from: https://github.com/KevinMusgrave/pytorch-metric-learning
         """
         self.indices = indices
         self.labels = labels
+        self.batch_size = batch_size
         self.M = M
         self.unique_labels = np.unique(labels)
         self.label_indices = self.get_label_indices(labels)
+        self.class_weights = class_weights
 
         self.list_size = len(indices)
         if self.M*self.unique_labels.shape[0] < self.list_size:
@@ -58,7 +60,14 @@ class CustomSampler(torch.utils.data.Sampler):
         # valid classes (classes that have items in them)
         self.valid_labels = np.unique(labels[indices])
         # whether or not to replace labels when picking classes
-        self.replace_labels = len(self.valid_labels) < batch_size/self.M
+        self.replace_labels = len(self.valid_labels) < int(np.ceil(self.list_size/self.M))
+
+        # if no weights given start uniform
+        if self.class_weights is None:
+            self.class_weights = np.ones(len(self.valid_labels)) / len(self.valid_labels)
+        else:
+            # normalize to equal to 1
+            self.class_weights /= np.sum(self.class_weights)
 
     def get_label_indices(self, labels):
         """
@@ -96,15 +105,27 @@ class CustomSampler(torch.utils.data.Sampler):
         iter_list = [0]*self.list_size
 
         # pick labels we will use (we need batch_size / M classes)
-        labels = np.random.choice(self.valid_labels, 
-                                  size=int(np.ceil(self.list_size/self.M)),
-                                  replace=self.replace_labels)
+        # labels = np.random.choice(self.valid_labels, 
+        #                           size=int(np.ceil(self.list_size/self.M))+1,
+        #                           replace=self.replace_labels,
+        #                           p=self.class_weights)
+
+        print(self.class_weights)
+
+        num_batches = int(np.ceil(self.list_size/self.batch_size))
+        labels = []
+        for _ in range(num_batches):
+            labels.append(np.random.choice(self.valid_labels,
+                                           size=int(np.ceil(self.batch_size/self.M)),
+                                           replace=len(self.valid_labels) < int(np.ceil(self.batch_size/self.M)),
+                                           p=self.class_weights))
+        labels = np.hstack(labels)
         
         for i in range(0, self.list_size, self.M):
             # now pick M random indices from that label and add to iter_list
-            iter_list[i:i+self.M] = np.random.choice(self.label_indices[labels[i]],
+            iter_list[i:i+self.M] = np.random.choice(self.label_indices[labels[i//self.M]],
                                                      size=self.M,
-                                                     replace=len(self.label_indices[labels[i]]) < self.M)
+                                                     replace=len(self.label_indices[labels[i//self.M]]) < self.M)
         return iter(iter_list)
 
     def __len__(self):
@@ -179,7 +200,8 @@ def get_dataloaders(dataset,
                     M=3,
                     labels=None,
                     train_labels=None,
-                    train_indices=None):
+                    train_indices=None,
+                    class_weights=None):
     """
     This function loads the h5 file and instantiates a FoodData object, it then builds dataloaders
     for the train set and returns necessary information.
@@ -211,7 +233,9 @@ def get_dataloaders(dataset,
     # initialize our samplers with the selected indices
     sampler = CustomSampler(M=M,
                             indices=train_indices,
-                            labels=labels)
+                            labels=labels,
+                            batch_size=batch_size,
+                            class_weights=class_weights)
 
     # now build the dataloaders with the selected samplers
     trainloader = DataLoader(

main.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from tqdm.auto import tqdm
 import psutil
 from io import BytesIO
+import copy
 
 class NewData(torch.utils.data.Dataset):
     def __init__(self, h5_path=None, transform=None):
@@ -15,44 +16,31 @@ class NewData(torch.utils.data.Dataset):
                                           applies transforms
         """
         self.transform = transform
+        self.im_paths = copy.deepcopy(image_paths)
+        self.labels = copy.deepcopy(labels)
 
     def __getitem__(self, index):
         """
         Method for pulling images and labels from the initialized HDF5 file
         """
-        #X = Image.open(image_paths[index]).convert('RGB')
-        if index in cached_indices:
-            X = Image.open(images[index]).convert('RGB')
-        else:
-            X = Image.open(image_paths[index]).convert('RGB')
-        y = labels[index]
+        X = Image.open(self.im_paths[index]).convert("RGB")
+        y = self.labels[index]
 
         if self.transform is not None:
             X = self.transform(X)
         return X, y
 
     def __len__(self):
-        return len(labels)
+        return len(self.labels)
 
 
 if __name__ == '__main__':
 
-
-    # data_224 = "data_224_v1"
-    # data_128 = "data_128_v2"
-
-    # # prepare the experiment
+    # prepare the experiment
     prepare_experiment()
-    # training_data_path = f"data/data/{data_224}.hdf5"
-    # if os.path.exists(training_data_path) is False:
-    #     download_from_s3(file_name=f"{data_224}.hdf5",
-    #                      destination="data/data",
-    #                      bucket="msc-thesis")
-
 
     path = "larger_data/"
     directories = os.listdir(path)
-    #directories = np.random.choice(directories, size=len(directories), replace=False)
 
     image_paths = []
     categories = []
@@ -69,6 +57,39 @@ if __name__ == '__main__':
                 except:
                     pass
 
+    # for CUB200
+    # import shutil
+    # import os
+
+    # images = {}
+    # #image_paths = list(images.keys())
+    # with open("ram/CUB_200_2011/images.txt", "r") as f:
+    #     for i in f.readlines():
+    #         index, im_path = i.split(" ")
+    #         images[index] = "ram/CUB_200_2011/images/" + im_path.strip("\n")
+
+    # train_test = {}
+    # with open("ram/CUB_200_2011/train_test_split.txt", "r") as f:
+    #     for i in f.readlines():
+    #         index, train = i.split(" ")
+    #         train_test[images[index].strip("\n")] = train
+
+    # image_paths = list(images.values())
+    # train = []
+    # val = []
+    # for i in range(len(image_paths)):
+    #     if train_test[image_paths[i]].strip("\n") == "1":
+    #         train.append(i)
+    #     else:
+    #         val.append(i)
+
+    # categories = []
+    # for im in image_paths:
+    #     categories.append(im.strip("ram/CUB_200_2011/images/").split("/")[0])
+
+
+    # np.savez("CUB_indices.npz", train=train, val=val, holdout=[])
+
     # now make encoded labels
     le = LabelEncoder()
     le.fit(categories)
@@ -76,60 +97,48 @@ if __name__ == '__main__':
 
     print(len(labels))
 
-    # load entire dataset into memory cause YEEEEEET
-    print("Cache 40gb of images")
-    #images = []
-    images = {}
-    cached_indices = set()
-    for i in tqdm(range(len(image_paths))):
-        if i % 5_000 == 0:
-            mem_percentage = psutil.virtual_memory().percent
-        if mem_percentage < 90:
-            with open(image_paths[i], 'rb') as f:
-                #images.append(BytesIO(f.read()))
-                images[i] = BytesIO(f.read())
-            #images.append(Image.open(image_paths[i]).convert('RGB'))
-            cached_indices.add(i)
-        else:
-            break
-
     # build the model
     model = ThreeStageNetwork(num_classes=len(np.unique(labels)),
-                              trunk_architecture="efficientnet-b0",
+                              trunk_architecture="resnet-50",
                               trunk_optim="adamW",
                               embedder_optim="adamW",
                               classifier_optim="adamW",
                               trunk_lr=1e-4,
                               embedder_lr=1e-3,
                               classifier_lr=1e-3,
-                              trunk_decay=0.8,
-                              embedder_decay=0.8,
-                              classifier_decay=0.8,
+                              trunk_decay=0.9,
+                              embedder_decay=0.9,
+                              classifier_decay=0.9,
                               log_train=True)
 
-    #model.load_weights("models/models.h5", load_classifier=False, load_optimizers=False)
+    #model.load_weights("models/models.h5", load_classifier=True, load_optimizers=False)
     model.setup_data(dataset=NewData,
-                     batch_size=282,
-                     load_indices=False,
+                     batch_size=160,
+                     load_indices=True,
                      num_workers=8,
-                     M=3,
-                     labels = labels,
-                     train_split=0.95)
+                     M=4,
+                     labels=labels,
+                     indices_path="logs/data_indices.npz",
+                     train_split=0.9)
 
     print(len(model.labels))
     print(len(np.unique(model.labels)))
     print(len(model.train_indices))
     model.train(n_epochs=10,
                 loss_ratios=[1,5,1,5],
+                class_weighting=False,
                 epoch_train=False,
                 epoch_val=True)
 
-    # let's get the embeddings and save those too for some visualization
-    model.save_all_logits_embeds("logs/logits_embeds.npz")
+    try:
+        # let's get the embeddings and save those too for some visualization
+        model.save_all_logits_embeds("logs/logits_embeds.npz")
+    except:
+        pass
 
     # finish experiment and zip up
     experiment_id = zip_files(["models", "logs"],
-                              experiment_id="test_knn_classifier")
+                              experiment_id="resnet18_test")
     upload_to_s3(file_name=f"experiment_{experiment_id}.zip",
                  destination=None,
                  bucket="msc-thesis")

model/knn.py
@@ -20,7 +20,7 @@ class TorchKNN():
         self.dimension = dimension
         self.cells_allocate = cells_allocate
         self.verbose = verbose
-        self.device = torch.device("gpu") if torch.cuda.is_available() else torch.device("cpu")
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
         # pre-allocate some cells for more efficient addition of future data
         if self.verbose >= 1:
@@ -162,7 +162,7 @@ def torch_get_top_k(bins, k=3):
     return torch.topk(bins, k=k).indices
 
 
-def knn_sim(embeddings, labels, k=3, local_normalization=True):
+def knn_sim(embeddings, labels, k=3, distance_weighted=True, local_normalization=True, num_classes=None):
     """
     Simulates kNN for every embedding. Basically operates under the principle
     that when we compute the pairwise distance we are computing the distance
@@ -172,21 +172,51 @@ def knn_sim(embeddings, labels, k=3, local_normalization=True):
 
     # precompute some required values, also looks cleaner
     batch_size = labels.shape[0]
-    num_classes = labels.max().item() + 1
+    if num_classes is None:
+        num_classes = labels.max().item() + 1
 
     # get the K nearest values distance weighted (basically torch.bincount with weighting)
     ans = torch.cdist(embeddings, embeddings) # get pairwise distance matrix to simulate kNN
     torch.diagonal(ans).fill_(float("Inf")) # so that we don't select that value
     dist, nearest = torch.topk(ans, k = k, largest=False) # get k closest to each value
-    x = torch.zeros([batch_size, num_classes]).cuda() # prepare a hacky way to implement bincount for multiD
-    x = x.scatter_add(1, labels[nearest], dist.reciprocal()) # compute the bincount with reciprocal distance
-    x = Variable(x.data, requires_grad=True)
+    if distance_weighted:
+        x = torch.zeros([batch_size, num_classes]).cuda() # prepare a hacky way to implement bincount for multiD
+        x = x.scatter_add(1, labels[nearest], dist.reciprocal()) # compute the bincount with reciprocal distance
+    else:
+        # if no distance weighting then simply return the bincount, basic kNN rule of local majority
+        x = torch.zeros([batch_size, num_classes]).cuda() # prepare a hacky way to implement bincount for multiD
+        x = x.scatter_add(1, labels[nearest], torch.ones([batch_size, k]).cuda()) # compute the bincount with reciprocal distance
 
-    if local_normalization:
+    # can't locally normalize without distance weighting, doesn't make sense.
+    if local_normalization is True and distance_weighted is True:
         # local normalization with smooth coefficient 1E-4
         x2 = torch.zeros([batch_size, num_classes]).cuda() # prepare a hacky way to implement bincount for multiD
         x2 = x2.scatter_add(1, labels[nearest], torch.ones([batch_size, k]).cuda())
         x = (x + 1e-4) / (x2 + 1) # divide local reciprocal distance sums by number of local occurances
 
-    #preds = torch.argmax(x, dim=1) # predictions for each value
+    return x
+
+
+def get_weights(preds, labels):
+    report = classification_report(preds.cpu().numpy(), labels.cpu().numpy(), output_dict=True)
+    f1_scores = [report[key]["f1-score"] for key in list(report.keys())[:-3]]
+    weights = torch.from_numpy(np.array(f1_scores) + 0.05).reciprocal()
+    weights /= torch.sum(weights)
+    return weights.numpy(), np.array([int(i) for i in list(report.keys())[:-3]])
+
+
+def impostor_weights(preds, labels, k, num_classes):
+
+    # dark wizardry to extract Nth column (labels) of each prediction
+    index = torch.stack([torch.zeros(len(labels), dtype=torch.int32).cuda(), labels]).T
+
+    # number of positive neighbors per class and impostors
+    num_positives = preds.gather(dim=1, 
+                                 index=index)[:,1]
+    num_impostors = torch.abs(num_positives - k)
+
+    # count number of impostors per label
+    x = torch.zeros(num_classes).cuda()
+    x = x.scatter_add(0, labels, num_impostors)
+
     return x

model/model.py
@@ -17,6 +17,7 @@ Third party libraries:
 import argparse
 import collections
 import numpy as np
+from sklearn.metrics import classification_report
 import functools
 
 # Import pytorch metric learning stuff
@@ -59,6 +60,7 @@ from .utils import calc_accuracy
 from .losses import *
 from data.hdf5_loader import *
 from data.autoaugment import ImageNetPolicy
+from .knn import knn_sim, get_weights, impostor_weights
 
 
 class Network(nn.Module):
@@ -66,6 +68,7 @@ class Network(nn.Module):
     def __init__(self, layer_sizes, neuron_fc=2048):
         super().__init__()
         self.classifier = nn.Sequential(
+            nn.ReLU(True),
             nn.Linear(layer_sizes[0], neuron_fc),
             nn.BatchNorm1d(neuron_fc, momentum=0.9),
             nn.ReLU(True),
@@ -446,22 +449,23 @@ class ThreeStageNetwork():
             if self.log_train:
                 np.savez(log_save_path + "/data_indices.npz", train=train_indices, val=val_indices, holdout=holdout_indices)
 
-        augmentations = data_augmentation(hflip=True,
-                                          crop=False,
-                                          colorjitter=True,
-                                          rotations=False,
-                                          affine=False,
-                                          imagenet=True)
+        self.augmentations = data_augmentation(hflip=True,
+                                               crop=True,
+                                               colorjitter=True,
+                                               rotations=False,
+                                               affine=False,
+                                               imagenet=True)
 
         trainloader, val_dataset = get_dataloaders(dataset=dataset,
                                                    h5_path=h5_path,
                                                    batch_size=batch_size,
                                                    num_workers=num_workers,
-                                                   augmentations=augmentations,
+                                                   augmentations=self.augmentations,
                                                    M=M,
                                                    labels=self.labels,
                                                    train_indices=np.repeat(train_indices, repeat_indices))
 
+        self.dataset = dataset
         self.trainloader = trainloader
         self.val_dataset = val_dataset
         self.train_indices = train_indices
@@ -471,11 +475,13 @@ class ThreeStageNetwork():
         self.log_save_path = log_save_path
         self.num_workers = num_workers
         self.M = M
+        self.repeat_indices = repeat_indices
 
 
     def train(self,
               n_epochs,
               loss_ratios=[1,1,1,3],
+              class_weighting=False,
               model_save_path="models",
               model_name="models.h5",
               epoch_train=False,
@@ -556,6 +562,13 @@ class ThreeStageNetwork():
                                 "Logging":0,
                                 "Total":0}
 
+            # initialize class weights to be uniform distribution if no class_weighting
+            if class_weighting:
+                # start in a smoothed fashion with 5 collisions each
+                class_weights = np.ones(self.num_classes) + 5
+            else:
+                class_weights = np.ones(self.num_classes) / self.num_classes
+
             with tqdm(total=int(n_iters)) as t:
                 start_t = time()
                 for i, data in enumerate(self.trainloader):
@@ -622,6 +635,19 @@ class ThreeStageNetwork():
 
                     scaler.update()
 
+                    if class_weighting:
+                        # compute batch label weightings
+                        preds = knn_sim(embeddings, labels, 
+                                        k=self.M,
+                                        distance_weighted=False,
+                                        local_normalization=False, 
+                                        num_classes=self.num_classes)
+                        weights = impostor_weights(preds, labels, k=self.M, num_classes=self.num_classes)
+                        #weights, associated_labels = get_weights(preds, labels)
+                        # moving average weight calculation (x[l] = x[l] + (new_data - x[l])/(i+1))
+                        class_weights += (weights.cpu().numpy() - class_weights)/(i+1)
+                        #class_weights[associated_labels] += (weights - class_weights[associated_labels])/(i+1)
+
                     # self.trunk_optimizer.step()
                     # self.embedder_optimizer.step()
                     # self.classifier_optimizer.step()
@@ -668,6 +694,10 @@ class ThreeStageNetwork():
                     t.update()
                     performance_dict["Logging"] += time() - time_check
 
+            # save class weights after each epoch
+            if class_weighting:
+                np.save(f"logs/class_weights_{i}.npy", class_weights)
+
             # build and save performance dictionary, keep in mind Load_Data is inaccurate due to prefetching
             performance_dict["Load_Data"] = np.sum(performance_dict[key] for key in performance_dict)
             performance_dict["Total"] = time() - start_t
@@ -678,6 +708,16 @@ class ThreeStageNetwork():
                 with open(self.log_save_path + "/performance.json", "w") as json_file:
                     json.dump(performance_dict, json_file)
             print(performance_dict)
+
+            # get the next dataloader based on class weights
+            trainloader, val_dataset = get_dataloaders(dataset=self.dataset,
+                                                       batch_size=self.batch_size,
+                                                       num_workers=self.num_workers,
+                                                       augmentations=self.augmentations,
+                                                       M=self.M,
+                                                       labels=self.labels,
+                                                       train_indices=np.repeat(self.train_indices, self.repeat_indices),
+                                                       class_weights=class_weights)
 
             if epoch_train is True:
                 # Train accuracy, embeddings and potential UMAP